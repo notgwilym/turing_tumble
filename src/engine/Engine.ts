027523ccdb13c89bea919a2c6ac2a446dc43aecd
Gwilym Hughes <gwilymrhughes@icloud.com>
2025-10-28 19:18:03 +0000

    add game loop (tick() and dropBall()) and state string output methods

--- src/engine/Engine.ts
+++ src/engine/Engine.ts
@@ -1,6 +1,7 @@
-import { Board } from "./Board";
+import { Board, CellType } from "./Board";
 import { Ball } from "./Ball";
 import { Piece } from "./pieces/Piece";
+import { Interceptor } from "./pieces/Interceptor";
 
 enum EngineState {
   INIT = 'INIT',
@@ -68,8 +69,51 @@ export class Engine {
     this.transition(StateTransition.INIT_DONE);
   }
 
-  public getState(): EngineState {
-    return this.state;
+  public getStateString(): string {
+    let state = "";
+    // current state
+    state += `Current State: ${this.state}\n`;
+    // current tick
+    state += `Current Tick: ${this.currentTick}\n`;
+    // left start queue
+    state += `Left Start Queue: [${this.leftStartQueue.map((ball) => ball.colour).join(", ")}]\n`;
+    // right start queue
+    state += `Right Start Queue: [${this.rightStartQueue.map((ball) => ball.colour).join(", ")}]\n`;
+    // board
+    state += `Board: \n`;
+    
+    for (let y = 0; y < this.board.getGrid().length; y++) {
+      let row = "";
+      for (let x = 0; x < this.board.getGrid()[y].length; x++) {
+        const cellType = this.board.getGrid()[y][x];
+        const piece = this.board.getPieceAt(x, y);
+        if (cellType === CellType.Blank) {
+          row += "[   ] ";
+        } else if (cellType === CellType.Peg) {
+          row += "[ . ] ";
+        }
+        else if (cellType === CellType.SlotPeg) {
+          if (piece) {
+            if (this.activeBalls.length == 1 && this.activeBalls[0].x === x && this.activeBalls[0].y === y) {
+              row += `[${piece.constructor.name.charAt(0)} ${this.activeBalls[0].colour.charAt(0).toUpperCase()}] `;
+            } else {
+              row += `[${piece.constructor.name.charAt(0)}  ] `;
+            }
+          } else {
+            row += "[ _ ] ";
+          }
+        }
+        else if (cellType === CellType.LeftExit) {
+          row += "[<- ] ";
+        }
+        else if (cellType === CellType.RightExit) {
+          row += "[ ->] ";
+        }
+      }
+      state += row + "\n";
+    }
+    
+    return state;
   }
 
   public getBoard(): Board {
@@ -82,17 +126,95 @@ export class Engine {
     }
     this.board.placePiece(piece);
   }
+
+  // remove piece
+
   // play
 
-  // step
   public step() {
     this.transition(StateTransition.STEP);
     // process one tick
+    this.tick();
   }
 
+  // process game logic for one tick
   private tick() {
+    if (this.state !== EngineState.RUNNING && this.state !== EngineState.FROZEN) {
+      throw new Error("Can only process tick in RUNNING or FROZEN state");
+    }
+
+    if (this.currentTick === 0) {
+      // introduce first ball
+      this.dropBall(this.board.getStartSide());
+      this.currentTick += 1;
+      return;
+    }
+
+    if (this.activeBalls.length != 1 ) {
+      throw new Error("No single active ball to process in tick.");
+    }
+    let currentBall = this.activeBalls[0];
+    let ballAtCellType = this.board.getCellType(currentBall.x, currentBall.y);
+
+    // if ball at a slotpeg, get the piece there and process interaction
+    if (ballAtCellType === CellType.SlotPeg) {
+      let pieceAtLocation = this.board.getPieceAt(currentBall.x, currentBall.y);
+      if (pieceAtLocation === null) {
+        throw new Error("No piece placed at slot where ball has reached.");
+      }
+      // sim terminates if ball hits interceptor
+      if (pieceAtLocation instanceof Interceptor) {
+        this.activeBalls.pop();
+        this.transition(StateTransition.TERMINAL_STATE);
+        return;
+      }
+
+      pieceAtLocation.handleBall(currentBall);
+    }
+    // else handle right and left exits
+    else if (ballAtCellType === CellType.RightExit) {
+      this.finishedBalls.push(this.activeBalls.pop()!);
+
+      let newBall = this.dropBall('right');
+      if (!newBall) {
+        this.transition(StateTransition.TERMINAL_STATE);
+      }
+    }
+    else if (ballAtCellType === CellType.LeftExit) {
+      this.finishedBalls.push(this.activeBalls.pop()!);
+
+      let newBall = this.dropBall('left');
+      if (!newBall) {
+        this.transition(StateTransition.TERMINAL_STATE);
+      }
+    }
+    else {
+      throw new Error("Ball is at invalid cell type.");
+    }
+
     this.currentTick += 1;
-    // process ball movements
+  }
+
+  private dropBall(side : 'left' | 'right'): Ball | undefined {
+    let nextBall: Ball | undefined;
+    if (side === 'left') {
+      nextBall = this.leftStartQueue.pop();
+      if (nextBall) {
+        nextBall.moveTo(this.board.getLeftEntryX(), 0);
+      }
+    }
+    else {
+      nextBall = this.rightStartQueue.pop();
+      if (nextBall) {
+        nextBall.moveTo(this.board.getRightEntryX(), 0);
+      }
+    }
+    
+    if (nextBall) {
+      this.activeBalls.push(nextBall);
+    }
+
+    return nextBall;
   }
 
   private populateStartQueue(colour: 'red' | 'blue', count: number): Ball[] {
@@ -107,12 +229,12 @@ export class Engine {
     const transitions = STATE_TRANSITIONS[this.state];
     
     if (!transitions || !(transition in transitions)) {
-      console.warn(`Invalid transition: ${transition} from ${this.state}`);
+      console.warn(`FAILED: Invalid transition '${transition}' from state ${this.state}'`);
       return false;
     }
 
     this.state = transitions[transition]!;
-    console.log(`Transitioned to state: ${this.state}`);
+    console.log(`SUCCESS: Transitioned to state '${this.state}' via '${transition}'`);
   }
 
 }
