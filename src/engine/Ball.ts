dc34c3659a10dc34c35f5ec7be769877978b31f2
Gwilym Hughes <gwilymrhughes@icloud.com>
2025-10-28 19:18:58 +0000

    refactor Ball.ts

--- src/engine/Ball.ts
+++ src/engine/Ball.ts
@@ -9,14 +9,16 @@ export class Ball {
         this.colour = colour;
         this.x = -1;
         this.y = -1;
+
+        // reds come in from left
         if (colour === 'red') {
             this.prev_x = -1;
             this.prev_y = -1;
-        } else if (colour === 'blue') {
+        } 
+        // blues from right
+        else {
             this.prev_x = 99999;    
             this.prev_y = -1;
-        } else {
-            throw new Error("Invalid ball colour");
         }
     }
 
@@ -25,5 +27,6 @@ export class Ball {
         this.prev_y = this.y;
         this.x = newX;
         this.y = newY;
+        console.log(`Ball moved to (${this.x}, ${this.y})`);
     }
 }
