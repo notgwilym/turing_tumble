9dec71790f3a39d3bd4093b1b764c89ab19e4c75
notgwilym <gwilymrhughes@gmail.com>
2026-01-29 09:58:33 +0000

    update vite path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@engine': path.resolve(__dirname, 'src/engine'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
 })
